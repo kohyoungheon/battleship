2fa190ea54ef4fd5c6d61964837d77e64b404afd
Young Koh <youngheon6@gmail.com>
2023-02-16 14:38:18 -0700

    feat: Define and test #place_ship

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -13,4 +13,13 @@ describe '#initialize' do
       expect(cell.empty?).to eq(true)
     end
   end
+
+  describe '#place_ship' do
+    it 'sets ship attribute to ship object' do
+      cell = Cell.new("A1")
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+      expect(cell.ship).to eq(cruiser)
+    end
+  end
 end
