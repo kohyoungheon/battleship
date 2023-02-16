88d710b67a2a8b9066f7fa3774ebd5451233147d
Young Koh <youngheon6@gmail.com>
2023-02-16 14:35:31 -0700

    feat: Define and test #empty?

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,7 +2,15 @@ require 'spec_helper'
 
 describe '#initialize' do
   it 'can initialize' do
-    cell = Cell.new
+    cell = Cell.new("A1")
     expect(cell).to be_instance_of(Cell)
+    expect(cell.coordinate).to eq("A1")
+  end
+
+  describe '#empty?' do
+    it 'returns boolean whether cell is empty' do
+      cell = Cell.new("A1")
+      expect(cell.empty?).to eq(true)
+    end
   end
 end
