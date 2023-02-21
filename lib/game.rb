97b3f2694a437cbf9efdd885978a188cb7743239
Isaac Thill <ithill22@turing.edu>
2023-02-21 15:19:10 -0700

    feat: merge

--- lib/game.rb
+++ lib/game.rb
@@ -26,6 +26,27 @@ class Game
       @computer.board.place(ship, valid?(ship))
     end
 
+    def computer_fire(cell)
+      @player.board.cells[cell].fire_upon
+      if @player.board.cells[cell].ship == nil
+        puts 'My shot on #{cell} was a miss.'
+      elsif @player.board.cells[cell].ship != nil
+        puts 'My shot on #{cell} was a hit.'
+        if @player.board.cells[cell].ship.sunk?
+          puts 'I sunker your #{@player.cells[cell].ship.name}!'
+        end
+      end
+    end
+  
+    def comp_picks_cell
+      cell = @player.board.cells.values.sample(1)
+      until cell(0).fire_upon? == false do
+        cell = @player.board.values.sample(1)
+      end
+      computer_fire(cell[0].coordinate)
+      cell[0].coordinate
+    end
+
   def player_placement
     puts "I have laid out my ships on the grid."
     puts "You now need to lay out your two ships."
