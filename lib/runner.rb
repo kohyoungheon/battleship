e842f0a9283d6a4032eeb189d4b2a2763c4147a6
Young Koh <youngheon6@gmail.com>
2023-02-21 14:36:17 -0700

    feature: Computer and Player ship placement

--- lib/runner.rb
+++ lib/runner.rb
@@ -3,11 +3,11 @@ require 'pry'
 require '../lib/ship'
 require '../lib/cell'
 require '../lib/board'
+require '../lib/game'
+require '../lib/player'
+require '../lib/computer'
 
-board = Board.new
-cruiser = Ship.new("Cruiser", 3) 
-subs = Ship.new("Sub", 2)
-board.place(cruiser, ["A1", "A2", "A3"])
-board.place(subs, ["D3", "D2"])
-puts board.render
-puts board.render(true)
+player = Player.new
+computer = Computer.new
+game = Game.new(player, computer)
+game.play
